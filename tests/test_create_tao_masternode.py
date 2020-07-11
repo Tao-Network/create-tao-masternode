76046ced12823c6a4ca1a40912b6aae3bf61d4b0
Bryce Weiner <bryce.weiner@alt.market>
2020-07-11 11:12:36 -0700

    Taoification

---
+++ tests/test_create_tao_masternode.py
@@ -0,0 +1,33 @@
+import yaml
+
+import pytest
+
+from create_tao_masternode import __version__
+from create_tao_masternode import main
+
+
+@pytest.fixture
+def runner():
+    from click.testing import CliRunner
+    runner = CliRunner()
+    return runner
+
+
+def test_version():
+    assert __version__ == '1.2.2'
+
+
+def test_produced_yml(runner):
+    input = (
+        "123456123456\n"
+        "123456123456\n"
+        "\n"
+        "/tmp\n"
+        "\n"
+        "\n"
+        "\n"
+    )
+    with runner.isolated_filesystem():
+        runner.invoke(main.entrypoint, ['test-node'], input=input)
+        with open('test-node/docker-compose.yml') as file:
+            assert yaml.load(file)
